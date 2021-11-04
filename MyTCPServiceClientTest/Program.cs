b68ac88ff13ff4d24a6042cd9df457c8709a6fe8
LDallakyan <90414146+LDallakyan@users.noreply.github.com>
2021-11-04 12:57:47 +0400

    Add tests.

---
+++ MyTCPServiceClientTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using TCPService;
+using TCPService.Interfaces;
+
+namespace MyTcpClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunClient();
+        }
+
+        static void RunClient()
+        {
+            using (IMyTCPClient client = new MyTCPClientFactory().CreateClient("127.0.0.1", 13005))
+            {
+                Console.WriteLine("Client_Test");
+                Thread.Sleep(100);
+                client.Connect();
+                client.Write("test", "Test_From_Client.");
+                client.Read("test");
+                client.Read("test1");
+                Thread.Sleep(300);
+                client.Write("test1", "Second_Tect_From_Client.");
+                client.Read("test1");
+                client.Disconnect();
+                Thread.Sleep(1000);
+                Console.WriteLine("Press Enter for exit.");
+                Console.Read();
+            }
+        }
+
+    }
+}
---
+++ MyTCPServiceServerTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using TCPService;
+using TCPService.Interfaces;
+
+namespace MyTcpServerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunServer();
+        }
+
+        static void RunServer()
+        {
+            using (IMyTCPServer server = new MyTCPServerFactory().CreateServer("127.0.0.1", 13005))
+            {
+                Console.WriteLine("Server_Test");
+                server.Start();
+                server.Read("test");
+                Thread.Sleep(300);
+                server.Write("test", "Test_From_Server.");
+                server.Read("test");
+                server.Read("test1");
+                server.Write("test1", "Second_From_Server.");
+                server.Read("test1");
+                Thread.Sleep(1000);
+                Console.WriteLine("Press Enter for exit.");
+                Console.Read();
+
+            }
+        }
+    }
+}
